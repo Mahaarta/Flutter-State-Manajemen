e23c51eb55cace9569b1f5358c1857a5ea99b8a9
Minata P Mahaarta <minataputra@gmail.com>
2023-01-13 18:16:36 +0700

    Changes: 1. Add file applicationColor.dart 	- for ChangeNotifier
    2. Change main.dart
    	- Refactor the text in the appBar, Switch, and Text to communicate with the applicationColor.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_manajement/application_color.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,38 +12,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          // ignore: prefer_const_constructors
-          title: Text(
-            "State Provider Managemtnt",
-            style: const TextStyle(color: Colors.black),
+      home: ChangeNotifierProvider<ApplicationColor>(
+        create: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+          backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+              builder: (context, applicationColor, child) => Text(
+                "State Provider Managemtnt",
+                style: TextStyle(color: applicationColor.color),
+              ),
+            ),
           ),
+          body: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Consumer<ApplicationColor>(
+                builder: (context, applicationColor, _) => AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    width: 100,
+                    height: 100,
+                    color: applicationColor.color),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      margin: const EdgeInsets.all(5), child: const Text("AB")),
+                  Consumer<ApplicationColor>(
+                    builder: (context, applicationColor, c_) => Switch(
+                      value: applicationColor.isLightBlue,
+                      onChanged: (newValue) {
+                        applicationColor.isLightBlue = newValue;
+                      },
+                    ),
+                  ),
+                  Container(
+                      margin: const EdgeInsets.all(5), child: const Text("LB"))
+                ],
+              )
+            ],
+          )),
         ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            AnimatedContainer(
-                duration: const Duration(milliseconds: 500),
-                width: 100,
-                height: 100,
-                color: Colors.lightBlue),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.all(5), child: const Text("AB")),
-                Switch(
-                  value: true,
-                  onChanged: (newValue) {},
-                ),
-                Container(
-                    margin: const EdgeInsets.all(5), child: const Text("LB"))
-              ],
-            )
-          ],
-        )),
       ),
     );
   }
