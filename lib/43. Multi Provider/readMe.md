dd4cead336dc1a5effca80b62f52595b0f7bfc5f
Minata P Mahaarta <minataputra@gmail.com>
2023-01-16 17:03:21 +0700

    Using bloc without library

---
+++ lib/43. Multi Provider/readMe.md
@@ -0,0 +1,2 @@
+link toturial 
+https://www.youtube.com/watch?v=nlvD964GqWI&list=PLZQbl9Jhl-VAg5F12Ri4lghrrLu2zfRyf&index=3
---
+++ lib/44. Bloc State Pure/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:state_manajement/44.%20Bloc%20State%20Pure/color_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ColorBloc bloc = ColorBloc();
+
+  @override
+  void dispose() {
+    super.dispose();
+    bloc.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                bloc.eventSink.add(ColorEvent.toAmber);
+              },
+              backgroundColor: Colors.amber,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                print("light blue pressed");
+                bloc.eventSink.add(ColorEvent.toLightBlue);
+              },
+              backgroundColor: Colors .lightBlue,
+            )
+          ],
+        ),
+        appBar: AppBar(
+          title: const Text("Pure BloC"),
+        ),
+        body: Center(
+          child: StreamBuilder(
+            stream: bloc.stateStream,
+            initialData: Colors.amber,
+            builder: (context, snapshot) {
+              return AnimatedContainer(
+                width: 100,
+                height: 100,
+                duration: const Duration(milliseconds: 500),
+                color: snapshot.data,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
